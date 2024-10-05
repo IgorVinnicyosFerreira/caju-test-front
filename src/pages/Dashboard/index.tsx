e916ddd4e9fce19eace7a164dfc815ec26bd1c9d
Igor Ferreira <vinicyosferreira@gmail.com>
2024-10-05 17:13:15 -0300

    feat: list admissions on load screen

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,26 @@
-import Collumns from "./components/Columns";
-import * as S from "./styles";
-import { SearchBar } from "./components/Searchbar";
+import Collumns from './components/Columns';
+import * as Styled from './styles';
+import { SearchBar } from './components/Searchbar';
+import { useQuery } from '@tanstack/react-query';
+import AdmissionsServiceFactory from '../../factories/services/admissionsServiceFactory';
 
 const DashboardPage = () => {
+  const admissionsService = AdmissionsServiceFactory.make();
+
+  const {data: admissions } = useQuery({
+    queryKey: ['registrations'],
+    queryFn: async () => {
+      const admissions = await admissionsService.listAdmissions();
+
+      return admissions;
+    }
+  });
+
   return (
-    <S.Container>
+    <Styled.Container>
       <SearchBar />
-      <Collumns registrations={[]} />
-    </S.Container>
+      <Collumns registrations={admissions} />
+    </Styled.Container>
   );
 };
 export default DashboardPage;
