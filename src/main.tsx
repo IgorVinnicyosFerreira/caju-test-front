e219d491e44ac4b3f9b78f8d7d25711a3c0758f7
Igor Ferreira <vinicyosferreira@gmail.com>
2024-10-05 17:13:50 -0300

    chore: update import path for queryClient config

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
-import queryClient from "./services/queryClient";
+import queryClient from "./configs/queryClient";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
