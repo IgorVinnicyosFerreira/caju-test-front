0fdeb44457b0236ad0b0bb639e8c8175981aeff8
Igor Ferreira <vinicyosferreira@gmail.com>
2024-10-05 18:13:10 -0300

    feat: search admissions by cpf

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,24 +1,35 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import Collumns from './components/Columns';
 import * as Styled from './styles';
 import { SearchBar } from './components/Searchbar';
-import { useQuery } from '@tanstack/react-query';
 import AdmissionsServiceFactory from '../../factories/services/admissionsServiceFactory';
 
 const DashboardPage = () => {
+  const [cpf, setCpf] = useState<string>();
+
   const admissionsService = AdmissionsServiceFactory.make();
 
   const {data: admissions } = useQuery({
-    queryKey: ['registrations'],
+    queryKey: ['registrations', cpf],
     queryFn: async () => {
-      const admissions = await admissionsService.listAdmissions();
+      const admissions = await admissionsService.listAdmissions({
+        filters: {
+          cpf
+        }
+      });
 
       return admissions;
     }
   });
 
+  const handleOnCPFSearch = (cpf: string) => {
+    setCpf(cpf)
+  }
+
   return (
     <Styled.Container>
-      <SearchBar />
+      <SearchBar onCPFSearch={handleOnCPFSearch} />
       <Collumns registrations={admissions} />
     </Styled.Container>
   );
